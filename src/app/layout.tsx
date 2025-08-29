23e05b15053328b7edbdce88cbdf7ea0124e9e3e
Natan Corcovia <natankorkov@gmail.com>
2025-08-29 18:41:38 -0300

    feat: add description on home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Estatísticas Brasileirão Série A",
-  description:
-    "Acompanhe elencos completos e estatísticas detalhadas dos jogadores da Série A do Campeonato Brasileiro, incluindo minutos, gols, assistências, finalizações, passes, desarmes e cartões.",
+  title: 'Estatísticas Brasileirão Série A',
+  description: 'Acompanhe elencos completos e estatísticas da Série A do Campeonato Brasileiro.',
 };
 
 export default function RootLayout({
@@ -25,11 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <div className="p-5">
+        <h2 className="mb-4 text-xl font-bold">Estatísticas Brasileirão Série A</h2>
+        <p className="font-medium">
+          Acompanhe elencos completos e estatísticas da Série A do Campeonato Brasileiro de 2023.
+        </p>
+      </div>
     </div>
   );
 }
